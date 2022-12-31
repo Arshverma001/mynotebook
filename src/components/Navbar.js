569445f75f6fb4079325d0d3df90be654414c1d9
Arshverma001 <87807771+Arshverma001@users.noreply.github.com>
2022-12-31 14:38:33 +0530

    final commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,86 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from "react";
 
-import {Link,useHistory,useLocation} from "react-router-dom";
+import { Link, useHistory, useLocation } from "react-router-dom";
 
 const Navbar = () => {
-  let history=useHistory()
-  let location=useLocation();
+  let history = useHistory();
+  let location = useLocation();
   useEffect(() => {
-     console.log(location.pathname)
+    console.log(location.pathname);
   }, [location]);
 
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    history.push("/login");
+  };
 
-  const handleLogout=()=>{
-    localStorage.removeItem('token')
-    history.push('/login')
-  }
-   
   return (
     <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
-  <div className="container-fluid">
-    <Link className="navbar-brand" to="/">iNotebook</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link className={`nav-link ${location.pathname==="/"}? "active" : ""`} aria-current="page" to="/">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link className={`nav-link ${location.pathname==="/about"}? "active" : "" `} to="/about">About</Link>
-        </li>
-      </ul>
-      { !localStorage.getItem('token')?
-        <form className="d-flex">
-      <Link type="button" className="btn btn-outline-light mx-1" to='/login'>Login</Link>
-      <Link type="button" className="btn btn-outline-light mx-1" to='/signup'>Signup</Link>
-      </form>:<button onClick={handleLogout} className='btn btn-outline-light'>Logout</button>}
-    </div>
-  </div>
-</nav>
-  )
-}
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          myNotebook
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link
+                className={`nav-link ${
+                  location.pathname === "/"
+                }? "active" : ""`}
+                aria-current="page"
+                to="/"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={`nav-link ${
+                  location.pathname === "/about"
+                }? "active" : "" `}
+                to="/about"
+              >
+                About
+              </Link>
+            </li>
+          </ul>
+          {!localStorage.getItem("token") ? (
+            <form className="d-flex">
+              <Link
+                type="button"
+                className="btn btn-outline-light mx-1"
+                to="/login"
+              >
+                Login
+              </Link>
+              <Link
+                type="button"
+                className="btn btn-outline-light mx-1"
+                to="/signup"
+              >
+                Signup
+              </Link>
+            </form>
+          ) : (
+            <button onClick={handleLogout} className="btn btn-outline-light">
+              Logout
+            </button>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
